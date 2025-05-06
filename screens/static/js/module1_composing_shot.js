ef03aee92de4730dc6ca2664f29f5ffcf65505ac
vinsonzzz <70662748+vinsonzzz@users.noreply.github.com>
2025-05-06 08:01:17 -0400

    Delete screens directory

--- screens/static/js/module1_composing_shot.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.full-details summary').on('click', function (e) {
-      const parent = $(this).parent();
-
-      // Collapse all others
-      $('.full-details').not(parent).removeAttr('open');
-    });
-  });
